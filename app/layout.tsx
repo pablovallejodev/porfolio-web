7dd210f36ea3caed861b580f5246137065e399ef
Pablo Vallejo <pablo@pablovallejo.dev>
2026-08-24 22:26:14 +0200

    chore: remove unused  SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Cormorant, Mulish, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const cormorant = Cormorant({
   variable: "--font-serif-cormorant",
@@ -57,7 +56,6 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col bg-background text-text">
         {children}
         <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   );
